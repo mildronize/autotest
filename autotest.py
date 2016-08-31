921d539fa0729270a2afc86ad91accffb7104bc7
Thada Wangthammang <mildronize@gmail.com>
2016-09-01 00:28:11 +0700

    refactor autotest into class

--- autotest.py
+++ autotest.py
@@ -3,115 +3,84 @@
 import os
 import ntpath
 import posixpath
-import sys
-import time
-from watchdog.observers import Observer
-from watchdog.events import PatternMatchingEventHandler
-
-# STYLE="startswith_test"
-# STYLE="endswith_test"
-UNITEST_DIR = "tests" # ending without /
-PACKAGE_NAME = "simple_calculator"
-STARTSWITH_TEST = True
-
-def process_filename_and_package_invert(file_path, os_name=None, startswith_test=True):
-    if os_name == 'nt':
-        seperater = "\\"
-    else:
-        seperater = "/"
-    if startswith_test:
-        test_style = "test_"
-    else:
-        test_style = "_test"
-    package = file_path.replace( UNITEST_DIR+seperater, "").replace(".py", "")[2:]
-    # print(package)
-    package = package.replace(seperater, ".").replace(test_style, "")
-    package = "{}.{}".format(PACKAGE_NAME, package)
-    # print(package) print(os.path.abspath(file_path))
-    test_file_path = file_path[2:]
-    return {
-        "test_file_path": test_file_path,
-        "package": package
-    }
-
-def process_filename_and_package(file_path, os_name=None, startswith_test=True):
-    if os_name == 'nt':
-        seperater = "\\"
-    else:
-        seperater = "/"
-
-    # file_path == "./simple_calculator/sub/calculator.py"
-    removed_head_tail = file_path[2:-3]
-    # removed_head_tail == "simple_calculator/sub/calculator"
-    package = removed_head_tail.replace(seperater, ".")
-    # package == "simple_calculator.sub.calculator"
-
-    # basename = os.path.basename(file_path)
-    if os_name == 'nt':
-        basename = ntpath.basename(file_path)
-    else:
-        basename = posixpath.basename(file_path)
-    # basename == "calculator.py"
-    prefix_path = file_path[2:].replace(basename, "")[:-1].replace(file_path.split(seperater)[1], "")
-    # prefix_path == "/sub"
-    if startswith_test:
-        test_basename = "test_{}".format(basename)
-    else:
-        test_basename = "{}_test.py".format(basename.replace(".py",""))
-    test_file_path = "{}{}{}{}".format(UNITEST_DIR, prefix_path, seperater, test_basename)
-    return {
-        "package": package,
-        "test_file_path": test_file_path
-    }
-
-def get_filename_and_package(file_path, os_name, startswith_test):
-    basename = os.path.basename(file_path)
-    if basename.startswith("test_") and basename.endswith(".py"):
-        return process_filename_and_package_invert(file_path, os_name, startswith_test)
-    if basename.endswith("_test.py"):
-        return process_filename_and_package_invert(file_path, os_name, startswith_test)
-    else:
-        return process_filename_and_package(file_path, os_name, startswith_test)
-
-def when_file_changed(filename):
-    # cls()
-    # filename = os.path.abspath(filename)
-    args = get_filename_and_package(filename, os.name, STARTSWITH_TEST)
-    nose = "nosetests"
-    options = "--rednose --with-coverage --cover-erase " \
-        "--cover-package={package} -v {test_file_path}".format(**args)
-    # -v verbose show a list of tests
-    cmd = nose + " " + options
-    print(cmd)
-    os.system(cmd)
-
-def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 
-class ModifiedHandler(PatternMatchingEventHandler):
-    patterns = ["*.py"]
+class PathProcessor:
 
-    def on_created(self, event):
-        when_file_changed(event.src_path)
+    def __init__(self, unittest_dir, package_name, startswith_test):
+        self.unittest_dir = unittest_dir
+        self.package_name = package_name
+        self.startswith_test = startswith_test
 
-    def on_any_event(self, event):
-        print(event)
+    def process_filename_and_package_invert(self, file_path, os_name=None, startswith_test=True):
+        if os_name == 'nt':
+            seperater = "\\"
+        else:
+            seperater = "/"
+        if startswith_test:
+            test_style = "test_"
+        else:
+            test_style = "_test"
+        package = file_path.replace(self.unittest_dir+seperater, "").replace(".py", "")[2:]
+        # print(package)
+        package = package.replace(seperater, ".").replace(test_style, "")
+        package = "{}.{}".format(self.package_name , package)
+        # print(package) print(os.path.abspath(file_path))
+        test_file_path = file_path[2:]
+        return {
+            "test_file_path": test_file_path,
+            "package": package
+        }
 
-    def on_modified(self, event):
-        when_file_changed(event.src_path)
+    def process_filename_and_package(self, file_path, os_name=None, startswith_test=True):
+        if os_name == 'nt':
+            seperater = "\\"
+        else:
+            seperater = "/"
 
-if __name__ == '__main__':
-    args = sys.argv[1:]
-    event_handler = ModifiedHandler()
-    observer = Observer()
-    observer.schedule(event_handler,
-                      path=args[1] if args else '.', recursive=True)
-    observer.start()
+        # file_path == "./simple_calculator/sub/calculator.py"
+        removed_head_tail = file_path[2:-3]
+        # removed_head_tail == "simple_calculator/sub/calculator"
+        package = removed_head_tail.replace(seperater, ".")
+        # package == "simple_calculator.sub.calculator"
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+        # basename = os.path.basename(file_path)
+        if os_name == 'nt':
+            basename = ntpath.basename(file_path)
+        else:
+            basename = posixpath.basename(file_path)
+        # basename == "calculator.py"
+        prefix_path = file_path[2:].replace(basename, "")[:-1].replace(file_path.split(seperater)[1], "")
+        # prefix_path == "/sub"
+        if startswith_test:
+            test_basename = "test_{}".format(basename)
+        else:
+            test_basename = "{}_test.py".format(basename.replace(".py",""))
+        test_file_path = "{}{}{}{}".format(self.unittest_dir, prefix_path, seperater, test_basename)
+        return {
+            "package": package,
+            "test_file_path": test_file_path
+        }
+
+    def get_filename_and_package(self, file_path, os_name, startswith_test):
+        basename = os.path.basename(file_path)
+        if basename.startswith("test_") and basename.endswith(".py"):
+            return self.process_filename_and_package_invert(file_path, os_name, startswith_test)
+        if basename.endswith("_test.py"):
+            return self.process_filename_and_package_invert(file_path, os_name, startswith_test)
+        else:
+            return self.process_filename_and_package(file_path, os_name, startswith_test)
+
+    def when_file_changed(self, filename):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        args = self.get_filename_and_package(filename, os.name,
+                self.startswith_test)
+        nose = "nosetests"
+        options = "--rednose --with-coverage --cover-erase " \
+            "--cover-package={package} -v {test_file_path}".format(**args)
+        # -v verbose show a list of tests
+        cmd = nose + " " + options
+        print(cmd)
+        os.system(cmd)
+
+
--- test/test_autotest.py
+++ test/test_autotest.py
@@ -1,103 +1,127 @@
 import unittest
 
-from autotest import *
+from autotest import PathProcessor
 
-class TestAutotest(unittest.TestCase):
 
-    def test_process_filename_and_package_invert(self):
+class TestProcessFilenameAndPackageInvert(unittest.TestCase):
 
-        # windows
+    def setUp(self):
+        self.pp = PathProcessor("tests", "simple_calculator", True)
+
+    def test_process_filename_and_package_invert_windows_1(self):
         filename = ".\\tests\\test_calculator.py"
-        self.assertEqual( {
+        self.assertEqual({
                 "test_file_path": "tests\\test_calculator.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package_invert(filename, 'nt'))
+            }, self.pp.process_filename_and_package_invert(filename, 'nt'))
+
+    def test_process_filename_and_package_invert_windows_2(self):
         filename = ".\\tests\\sub\\test_calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests\\sub\\test_calculator.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package_invert(filename, 'nt'))
+            }, self.pp.process_filename_and_package_invert(filename, 'nt'))
 
+    def test_process_filename_and_package_invert_windows_3(self):
         filename = ".\\tests\\calculator_test.py"
         self.assertEqual( {
                 "test_file_path": "tests\\calculator_test.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package_invert(filename, 'nt', False))
+            }, self.pp.process_filename_and_package_invert(filename, 'nt', False))
+
+    def test_process_filename_and_package_invert_windows_4(self):
         filename = ".\\tests\\sub\\calculator_test.py"
         self.assertEqual( {
                 "test_file_path": "tests\\sub\\calculator_test.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package_invert(filename, 'nt', False))
+            }, self.pp.process_filename_and_package_invert(filename, 'nt', False))
 
-        # Unix
+    def test_process_filename_and_package_invert_unix_1(self):
         filename = "./tests/test_calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests/test_calculator.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package_invert(filename))
+            }, self.pp.process_filename_and_package_invert(filename))
 
+    def test_process_filename_and_package_invert_unix_2(self):
         filename = "./tests/sub/test_calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests/sub/test_calculator.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package_invert(filename))
+            }, self.pp.process_filename_and_package_invert(filename))
+
+    def test_process_filename_and_package_invert_unix_3(self):
         filename = "./tests/calculator_test.py"
         self.assertEqual( {
                 "test_file_path": "tests/calculator_test.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package_invert(filename, startswith_test=False))
+            }, self.pp.process_filename_and_package_invert(filename, startswith_test=False))
 
+    def test_process_filename_and_package_invert_unix_4(self):
         filename = "./tests/sub/calculator_test.py"
         self.assertEqual( {
                 "test_file_path": "tests/sub/calculator_test.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package_invert(filename, startswith_test=False))
+            }, self.pp.process_filename_and_package_invert(filename, startswith_test=False))
 
-    def test_process_filename_and_package(self):
-        # windows
+class TestProcessFilenameAndPackage(unittest.TestCase):
+
+    def setUp(self):
+        self.pp = PathProcessor("tests", "simple_calculator", True)
+
+    def test_process_filename_and_package_windows_1(self):
         filename = ".\\simple_calculator\\calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests\\test_calculator.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package(filename, 'nt'))
+            }, self.pp.process_filename_and_package(filename, 'nt'))
+
+    def test_process_filename_and_package_windows_2(self):
         filename = ".\\simple_calculator\\sub\\calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests\\sub\\test_calculator.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package(filename, 'nt'))
+            }, self.pp.process_filename_and_package(filename, 'nt'))
 
+    def test_process_filename_and_package_windows_3(self):
         filename = ".\\simple_calculator\\calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests\\calculator_test.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package(filename, 'nt', False))
+            }, self.pp.process_filename_and_package(filename, 'nt', False))
+
+    def test_process_filename_and_package_windows_4(self):
         filename = ".\\simple_calculator\\sub\\calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests\\sub\\calculator_test.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package(filename, 'nt', False))
+            }, self.pp.process_filename_and_package(filename, 'nt', False))
 
-        # Unix
+    def test_process_filename_and_package_unix_1(self):
         filename = "./simple_calculator/calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests/test_calculator.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package(filename))
+            }, self.pp.process_filename_and_package(filename))
+
+    def test_process_filename_and_package_unix_2(self):
         filename = "./simple_calculator/sub/calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests/sub/test_calculator.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package(filename))
+            }, self.pp.process_filename_and_package(filename))
+
+    def test_process_filename_and_package_unix_3(self):
         filename = "./simple_calculator/calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests/calculator_test.py",
                 "package": "simple_calculator.calculator"
-            }, process_filename_and_package(filename, startswith_test=False))
+            }, self.pp.process_filename_and_package(filename, startswith_test=False))
+
+    def test_process_filename_and_package_unix_4(self):
         filename = "./simple_calculator/sub/calculator.py"
         self.assertEqual( {
                 "test_file_path": "tests/sub/calculator_test.py",
                 "package": "simple_calculator.sub.calculator"
-            }, process_filename_and_package(filename, startswith_test=False))
+            }, self.pp.process_filename_and_package(filename, startswith_test=False))
 
-    def test_get_filename_and_package(self):
-        assert True
